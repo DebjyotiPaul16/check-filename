b01827c26197b50d7869bba1437aa61e8700ce07
Rakesh Sharma <rakesh.sharma@learningmate.com>
2015-11-12 19:49:23 +0530

    updated

--- bin/main.js
+++ bin/main.js
@@ -11,13 +11,31 @@ var lower_case = ''
 	,	lower_case_with_underscore = ''
 	,	camel_case = ''
 	, upper_case = ''
-	,	exclude = ['node_modules', 'bower_component', '.git', '.svn'];
+	,	exclude = ['node_modules', 'bower_component', '.git', '.svn', '.hg'];
 
 cmd
   .version(pkg.version)
 	.description(pkg.description)
-  .option('-c, --custom', 'Format type to check', lower_case)
-	.option('-e, --exclude', 'Format type to check', exclude)
+	.usage('[--path][--custom-regex][--exclude][--use-gitignore]')
+  .option('-c, --custom-regex', 'Format type to check.', lower_case)
+	.option('-e, --exclude', 'Files or Folder to exclude. ', exclude)
+	.option('-i, --use-gitignore', 'Don\'t use ".gitignore"', true)
   .option('-p, --path <folder path>', 'Folder path to run a filename check', './')
   .parse(process.argv);
 
+
+
+//debug notes
+console.log("Default args:-")
+if(cmd.customRegex){
+	console.log(cmd.cutomRegex);
+}
+if(cmd.exclude){
+	console.log(cmd.exclude);
+}
+if(cmd.useGitignore){
+	console.log(cmd.useGitignore);
+}
+if(cmd.path){
+	console.log(cmd.path);
+}
