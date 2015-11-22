7be174dfd3a0db4996bc2302e2dfed454ea45311
Rakesh Sharma <raks437@gmail.com>
2015-11-22 18:58:58 +0530

    updated

--- bin/main.js
+++ bin/main.js
@@ -6,48 +6,62 @@
 
 var fs = require('fs')
 	, path = require('path')
-	,	pkg = require( path.join(__dirname, '../package.json') )
+	, pkg = require(path.join(__dirname, '../package.json'))
 	, cmd = require('commander');
 
 var lower_case = ''
-	,	lower_case_with_hyphen = ''
-	,	lower_case_with_underscore = ''
-	,	camel_case = ''
+	, lower_case_with_hyphen = ''
+	, lower_case_with_underscore = ''
+	, camel_case = ''
 	, upper_case = ''
-	,	exclude = ['node_modules', 'bower_component', '.git', '.svn', '.hg'];
+	, exclude = ['node_modules', 'bower_component', '.git', '.svn', '.hg'];
 
 cmd
   .version(pkg.version)
 	.description(pkg.description)
 	.usage('[--path][--custom-regex][--exclude][--use-gitignore]')
   .option('-c, --custom-regex', 'format type to check.', lower_case)
-	.option('-e, --exclude', 'files or Folder to exclude. ', exclude)
 	.option('-i, --use-gitignore', 'don\'t use ".gitignore"', true)
-  .option('-p, --path <folder path>', 'folder path to run a filename check', './')
+	.option('-x, --exclude', 'files or Folder to exclude. ', exclude)
+  .option('-p, --path <folder path>', 'folder path to run a filename check', process.cwd())
   .parse(process.argv);
 
-var checkFilename = module.exports = function(){
+var checkFileName = module.exports = {
+	_this: this,
+	Folders: [],
+	Files: [],
+	readAllFiles: function (root) {
+		fs.readdir(root, function (error, files) {
+			files.forEach(function (elem, index) {
+				if (fs.lstatSync(path.join(root, elem)).isDirectory()) {
+					checkFileName.Folders.push(elem);
+					checkFileName.readAllFiles(path.join(root, elem));
+				}
+				else {
+					checkFileName.Files.push(elem);
+				}
+			});
+		});
+		console.log('Folders', checkFileName.Folders.length);
+		console.log('Files', checkFileName.Files.length);
+	},
+	readGitIgnore:function(){
 
-	return this;
-};
-
-var getPath = function(){
-
+	}
 };
 
 //debug notes
-console.log("Default args:-")
-console.log("Current dir:",__dirname);
-if(cmd.customRegex){
+console.log("Default args:-----------------------------------------------------------")
+if (cmd.customRegex) {
 	console.log(cmd.cutomRegex);
-
 }
-else if(cmd.exclude){
+else if (cmd.exclude) {
 	console.log(cmd.exclude);
 }
-else if(cmd.useGitignore){
+else if (cmd.useGitignore) {
 	console.log(cmd.useGitignore);
 }
-else if(cmd.path){
-	console.log("Path: "+cmd.path);
+else if (cmd.path) {
+	console.log("Path: " + cmd.path);
+	checkFileName.readAllFiles(process.cwd());
 }
