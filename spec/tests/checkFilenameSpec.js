13bd7f0a9258f7fc3318f355a6279e4426dbcfce
Rakesh Sharma <raks437@gmail.com>
2015-11-22 19:17:59 +0530

    updated test

--- spec/tests/checkFilenameSpec.js
+++ spec/tests/checkFilenameSpec.js
@@ -1,7 +1,25 @@
-describe('Check Filename CLI app',function(){
-	describe('checkFilename module',function(){
-		it('exists',function(){
-			expect(checkFilename).toBeDefined();
+var fs = require('fs')
+	, path = require('path')
+	, pkg = require(path.join(__dirname, '../../package.json'))
+
+var checkFileName = require('../../bin/main.js')
+
+describe('Check Filename CLI app ::', function () {
+	describe('checkFilename module', function () {
+		it('exists', function () {
+			expect(checkFileName).toBeDefined()
+		})
+
+		it('has list of folders', function () {
+			expect(checkFileName.Folders).toBeDefined()
+
+			expect(checkFileName.Folders).toBe([]);
+		})
+
+		it('has list of files', function () {
+			expect(checkFileName.Files).toBeDefined()
+
+			expect(checkFileName.Files).toBe([])
 		})
 	})
 })
