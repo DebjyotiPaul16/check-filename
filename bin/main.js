12506689c8474d69e1ed632b96e0e4c2a02c1fbc
Niraj Kumar Chauhan <niraj.1991@gmail.com>
2015-11-24 23:00:16 +0530

    lower case check logic added

--- bin/main.js
+++ bin/main.js
@@ -7,35 +7,42 @@ var fs = require('fs')
 	, cmd = require('commander')
 	,	chalk = require('chalk');
 
-var lower_case = ''
+var lower_case = /[a-z0-9]/
 	, lower_case_with_hyphen = ''
 	, lower_case_with_underscore = ''
 	, camel_case = ''
-	, upper_case = ''
+	, upper_case = /[A-Z0-9]/
 	, exclude = ['node_modules', 'bower_component', '.git', '.svn', '.hg'];
 
 cmd
-  .version(pkg.version)
+  	.version(pkg.version)
 	.description(pkg.description)
 	.usage('[--path][--custom-regex][--exclude][--use-gitignore]')
-  .option('-c, --custom-regex', 'format type to check.', lower_case)
+  	.option('-c, --custom-regex', 'format type to check.', lower_case)
 	.option('-i, --use-gitignore', 'don\'t use ".gitignore"', true)
 	.option('-x, --exclude', 'files or Folder to exclude. ', exclude)
-  .option('-p, --path <folder path>', 'folder path to run a filename check', process.cwd())
-  .parse(process.argv);
+  	.option('-p, --path <folder path>', 'folder path to run a filename check', process.cwd())
+  	.parse(process.argv);
 
 var checkFileName = module.exports = {
 	Folders: [],
 	Files: [],
+	files_with_error: [],
+	folders_with_error: [],
 
 	readAllFiles: function (srcPath) {
 		fs.readdir(srcPath, function (error, files) {
 			files.forEach(function (elem, index) {
 				if (fs.lstatSync(path.join(srcPath, elem)).isDirectory()) {
+					if (!checkFileName.isLowerCase(elem))
+						checkFileName.folders_with_error.push(elem);
 					checkFileName.Folders.push(elem);
 					checkFileName.readAllFiles(path.join(srcPath, elem));
 				}
 				else {
+					if (!checkFileName.isLowerCase(elem))
+						checkFileName.files_with_error.push(elem);
+
 					checkFileName.Files.push(elem);
 				}
 			});
@@ -49,12 +56,20 @@ var checkFileName = module.exports = {
 		if (checkFileName.timer) {
 			clearTimeout(checkFileName.timer);
 			checkFileName.timer = setTimeout(function () {
-				console.log("=============");
-				console.log(chalk.blue('Folders:'), chalk.red(checkFileName.Folders.length));
-				console.log(chalk.blue('Files:'), chalk.red(checkFileName.Files.length));
-				console.log("=============");
+				console.log("==============================================");
+				console.log(chalk.blue('Total Folders Scanned:'), chalk.white(checkFileName.Folders.length));
+				console.log(chalk.blue('Total Files Scanned:'), chalk.white(checkFileName.Files.length));
+				console.log("==============================================");
+				console.log(chalk.green('Scan Status:\n'));
+				console.log(chalk.red('Files (lower case violation):'), chalk.red(checkFileName.files_with_error.length));
+				console.log(chalk.red('Folders (lower case violation):'), chalk.red(checkFileName.folders_with_error.length));
+				console.log("==============================================");
 			}, 500);
 		}
+	},
+
+	isLowerCase: function (stringToCheck) {
+		return lower_case.test(stringToCheck);
 	}
 };
 
