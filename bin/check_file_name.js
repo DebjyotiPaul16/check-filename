acb2648374829356b15e148d85b4fda47c9a3223
Rakesh Sharma <raks437@gmail.com>
2015-11-26 23:53:33 +0530

    updated

---
+++ bin/check_file_name.js
@@ -0,0 +1,18 @@
+;'use strict';
+var checkFileName = function(srcPath){
+
+};
+
+checkFileName.prototype.displayResults = function(){
+
+};
+
+checkFileName.prototype.matchCase = function(matcherRegex){
+
+};
+
+checkFileName.prototype.isLowerCase = function(){
+
+};
+
+module.exports = checkFileName;
